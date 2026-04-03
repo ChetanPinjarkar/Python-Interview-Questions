fe476951a1b074deeaa468c9cf6601bc41a249c0
Chetan Pinjarkar <chetanpinjarkar@gmail.com>
2026-04-03 23:27:46 +0530

    Implement parentheses validation function
    Add a function to validate parentheses in a string.

--- validateParentheses.py
+++ validateParentheses.py
@@ -1,3 +1,28 @@
+def paranth_valid(s):
+  print(s)
+  if len(s)%2!=0:
+    return False
+
+  stack = []
+  matching = {']':'[', '}':'{', ')':'('}
+
+  for char in s:
+    if char in matching.values():
+      stack.append(char)
+    
+    elif not stack or stack.pop() != matching[char]:
+      return False
+  return not stack
+
+
+print(paranth_valid("([[]]{"))
+print(paranth_valid("[{()}]"))
+print(paranth_valid("[()()]{}"))
+print(paranth_valid("([]"))
+print(paranth_valid("([{]})"))
+
+
+#### Alternative ####
 def validate(s):
     if len(s)<=1:
         return False        
